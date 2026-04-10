f6b16be7de3d5f0a7cc2e8bc59ab560c75c70799
Kacper <kwlodarek@gmail.com>
2026-04-10 15:41:59 +0200

    CREATE: Create workshop activities

---
+++ test.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+
+const BASE_URL = __ENV.BASE_URL || 'http://localhost:3333';
+
+export default function () {
+  const restrictions = {
+    maxCaloriesPerSlice: 500,
+    mustBeVegetarian: false,
+    excludedIngredients: ['onions'],
+    excludedTools: ['knife'],
+    maxNumberOfToppings: 6,
+    minNumberOfToppings: 2,
+  };
+
+   let res = http.post(`${BASE_URL}/api/pizza`, JSON.stringify(restrictions), {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'token abcdef0123456789',
+    },
+  });
+
+  console.log(`${res.json().pizza.name} (${res.json().pizza.ingredients.length} ingredients`);
+}
